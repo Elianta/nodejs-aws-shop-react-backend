8b71a9864c3962e9f6c546d6be93a35087a9c3d7
a.kashina <a.kashina@websecret.by>
2025-02-19 17:59:39 +0100

    feat: initialize product service with basic structure and configuration

---
+++ product-service/src/functions/get-product-by-id/handler.ts
@@ -0,0 +1,3 @@
+export const handler = async () => {
+  //TODO: Implement the handler function to return a product by id
+};
---
+++ product-service/src/functions/get-products-list/handler.ts
@@ -0,0 +1,3 @@
+export const handler = async () => {
+  //TODO: Implement the handler function to return a list of products
+};
