60af08d5e9fd0a34dc286081e6d2e1b0f23fd05d
a.kashina <a.kashina@websecret.by>
2025-02-20 10:19:27 +0100

    feat: add get product by ID functionality

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -24,6 +24,15 @@ export class ProductServiceStack extends cdk.Stack {
       layers: [productsLayer],
     });
 
+    const getProductById = new lambda.Function(this, "getProductById", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler.handler",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "../dist/functions/get-product-by-id")
+      ),
+      layers: [productsLayer],
+    });
+
     // Create API Gateway
     const api = new apigateway.RestApi(this, "ProductsApi", {
       restApiName: "Products Service",
@@ -39,5 +48,8 @@ export class ProductServiceStack extends cdk.Stack {
       "GET",
       new apigateway.LambdaIntegration(getProductsList)
     );
+
+    const product = products.addResource("{productId}");
+    product.addMethod("GET", new apigateway.LambdaIntegration(getProductById));
   }
 }
--- product-service/src/functions/get-product-by-id/handler.ts
+++
@@ -1,3 +0,0 @@
-export const handler = async () => {
-  //TODO: Implement the handler function to return a product by id
-};
