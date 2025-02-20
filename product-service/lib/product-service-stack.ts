cb91335c650a5033e109e8b2b66ea391d8e1de5a
a.kashina <a.kashina@websecret.by>
2025-02-20 10:17:01 +0100

    feat: implement product listing functionality

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -1,16 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productsLayer = new lambda.LayerVersion(this, "ProductsLayer", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../dist/layers")),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      description: "Products mock data layer",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Lambda functions
+    const getProductsList = new lambda.Function(this, "getProductsList", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler.handler",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "../dist/functions/get-products-list")
+      ),
+      layers: [productsLayer],
+    });
+
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, "ProductsApi", {
+      restApiName: "Products Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    // Create resources and methods
+    const products = api.root.addResource("products");
+    products.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getProductsList)
+    );
   }
 }
