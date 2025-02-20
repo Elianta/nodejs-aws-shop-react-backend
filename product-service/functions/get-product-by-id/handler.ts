085f8dcc8b979b1b92ebc178208f02301de922c6
a.kashina <a.kashina@websecret.by>
2025-02-20 10:19:27 +0100

    feat: add get product by ID functionality

---
+++ product-service/functions/get-product-by-id/handler.ts
@@ -0,0 +1,38 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { products } from "/opt/nodejs/products";
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  try {
+    const productId = event.pathParameters?.productId;
+    const product = products.find((p) => p.id === productId);
+
+    if (!product) {
+      return {
+        statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({ message: "Product not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
+};
