2a28d43c19b5b248d9796f3d2777f1f5e5ef1368
a.kashina <a.kashina@websecret.by>
2025-04-17 14:42:23 +0200

    feat: add bff-service

---
+++ bff-service/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true, // Make config available everywhere
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
