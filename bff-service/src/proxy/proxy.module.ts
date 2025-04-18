81273b0fe8d8ea692cad8f6d2aacaa40b2f30ed8
a.kashina <a.kashina@websecret.by>
2025-04-18 15:21:33 +0200

    feat(proxy-service): implement caching mechanism for GET /products

--- bff-service/src/proxy/proxy.module.ts
+++ bff-service/src/proxy/proxy.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ProxyController } from './proxy.controller';
 import { ProxyService } from './proxy.service';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    CacheModule.register({
+      ttl: 2 * 60 * 1000, // 2 minutes
+    }),
+  ],
   controllers: [ProxyController],
   providers: [ProxyService],
 })
